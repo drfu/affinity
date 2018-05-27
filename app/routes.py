21102d1a97ada62b95c4dc8caa571746eff84fe3
Montgomery <r621362@TRG519305.domain>
2018-05-27 09:19:45 -0700

    just experimenting for now ( new to Jinja2 I am)

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Timbers"
+    user = {'username': 'kermitt'}
+    posts = [
+        {
+            'author': {'username': 'drfu'},
+            'body': 'Finch'
+        },
+        {
+            'author': {'username': 'Momos'},
+            'body': 'Example of a loop w/ flask and jinja2'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
